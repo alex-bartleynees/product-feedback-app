21c521b459c9931ea5deccc96e25a1de80ee3eee
alex-bartleynees <alexbartleynees@gmail.com>
2022-05-21 13:17:59 +1200

    Add header-component

---
+++ apps/client/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'product-feedback-app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
