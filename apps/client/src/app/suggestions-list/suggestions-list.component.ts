bdc105dbfdef8699ef92b9ad4ac259b60fd986c2
alex-bartleynees <alexbartleynees@gmail.com>
2022-05-21 13:18:20 +1200

    Add suggestions list

---
+++ apps/client/src/app/suggestions-list/suggestions-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'product-feedback-app-suggestions-list',
+  templateUrl: './suggestions-list.component.html',
+  styleUrls: ['./suggestions-list.component.scss'],
+})
+export class SuggestionsListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
