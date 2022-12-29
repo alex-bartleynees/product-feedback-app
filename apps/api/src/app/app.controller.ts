65f1b9acaa4a6b3cfdebfed1988ed66c57e81c13
Alexander Bartley Nees <alexbartleynees@gmail.com>
2022-12-29 21:01:00 +1300

    Add mock data to nest js

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,15 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { Message } from '@product-feedback-app/api-interfaces';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Suggestion } from '@product-feedback-app/api-interfaces';
 
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('suggestions')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello')
-  getData(): Message {
+  @Get()
+  getData(): Suggestion[] {
     return this.appService.getData();
+  }
+
+  @Post()
+  create(@Body() suggestion: Suggestion): Suggestion {
+    return this.appService.create(suggestion);
+  }
+
+  @Put('/:id')
+  update(@Body() suggestion: Suggestion): Suggestion {
+    return this.appService.update(suggestion);
+  }
+
+  @Delete('/:id')
+  delete(@Body() id: number): number {
+    return this.appService.delete(id);
   }
 }
